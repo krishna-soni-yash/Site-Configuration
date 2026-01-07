e9d411b7cf6acce98fb90787614ec4e77a083fdc
krishna.soni@yash.com <krishna.soni@yash.com>
2026-01-07 17:07:43 +0530

    Code Review Defects list internal names changed

--- src/extensions/schema/List Provision/lists/CodeReviewDefects.ts
+++ src/extensions/schema/List Provision/lists/CodeReviewDefects.ts
@@ -1,238 +1,208 @@
-/*eslint-disable*/
 import { SPFI } from "@pnp/sp";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
 import "@pnp/sp/fields";
 import "@pnp/sp/views";
 import {
-    ensureListProvision,
-    FieldDefinition,
-    ListProvisionDefinition
+	ensureListProvision,
+	FieldDefinition,
+	ListProvisionDefinition
 } from "../GenericListProvision";
 import { RequiredListsProvision } from "../RequiredListProvision";
 
 const LIST_TITLE = RequiredListsProvision.CodeReviewDefects;
 
 type CodeReviewDefectsFieldName =
-    | "Code File Cla"
-    | "Code File Cla0"
-    | "Code File -"
-    | "Reviewer Name"
-    | "Identified Date"
-    | "Defect Type"
-    | "Defect Classification"
-    | "Defect Description"
-    | "Code Review Checklis"
-    | "Severity"
-    | "Defect Status"
-    | "Review Results"
-    | "Defect Origin Phase"
-    | "Impacted Components"
-    | "Correction / C"
-    | "Planned Closure Date"
-    | "Actual Closure Date"
-    | "Review Iteration Num"
-    | "Review Completion Da"
-    | "Location of defect ("
-    | "Remarks";
-type CodeReviewDefectsViewField = CodeReviewDefectsFieldName;
+	| "CodeFileClassName"
+	| "CodeFileClassSize"
+	| "CodeFileAuthorDeveloperName"
+	| "ReviewerName"
+	| "ReviewIterationNumber"
+	| "IdentifiedDate"
+	| "DefectType"
+	| "DefectClassification"
+	| "CodeReviewChecklist"
+	| "Severity"
+	| "DefectStatus"
+	| "ReviewResults"
+	| "DefectOriginPhase"
+	| "ImpactedComponents"
+	| "CorrectionCorrectiveAction"
+	| "PlannedClosureDate"
+	| "ActualClosureDate"
+	| "LocationOfDefect"
+	| "Remarks";
 
-function buildFieldDefinitions(): FieldDefinition<CodeReviewDefectsFieldName>[] {
-    return [
-        {
-            internalName: "Code File Cla",
-            schemaXml: `<Field Type='Text' Name='Code File Cla' StaticName='Code File Class' DisplayName='Code File/ Class Name' MaxLength='255' />`
-        }
-        ,
-        {
-            internalName: "Code File Cla0",
-            schemaXml: `<Field Type='Text' Name='Code File Cla0' StaticName='Code File Cla0' DisplayName='Code File/ Class Size (Lines of Code)' MaxLength='255' />`
-        }
-        ,
-        {
-            internalName: "Code File -",
-            schemaXml: `<Field Type='User' Name='Code File -' StaticName='Code File -' DisplayName='Code File - Author/ Developer Name(s)' UserSelectionMode='PeopleOnly' />`
-        }
-        ,
-        {
-            internalName: "Reviewer Name",
-            schemaXml: `<Field Type='User' Name='Reviewer Name' StaticName='Reviewer Name' DisplayName='Reviewer Name' UserSelectionMode='PeopleOnly' />`
-        }
-        ,
-        {
-            internalName: "Identified Date",
-            schemaXml: `<Field Type='DateTime' Name='Identified Date' StaticName='Identified Date' DisplayName='Identified Date' Format='DateOnly' />`
-        }
-        ,
-        {
-            internalName: "Defect Type",
-            schemaXml: `<Field Type='Choice' Name='Defect Type' StaticName='Defect Type' DisplayName='Defect Type' Format='Dropdown'><CHOICES><CHOICE>Function</CHOICE><CHOICE>Interface</CHOICE><CHOICE>Checking</CHOICE><CHOICE>Assignment</CHOICE><CHOICE>Timing/ Serialization</CHOICE><CHOICE>Build/ Package</CHOICE><CHOICE>Documentation</CHOICE><CHOICE>Algorithm</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Defect Classification",
-            schemaXml: `<Field Type='Choice' Name='Defect Classification' StaticName='Defect Classification' DisplayName='Defect Classification' Format='Dropdown'><CHOICES><CHOICE>Design issues</CHOICE><CHOICE>Data Validation</CHOICE><CHOICE>Logical</CHOICE><CHOICE>Computational</CHOICE><CHOICE>User Interface Presentation</CHOICE><CHOICE>Input/output</CHOICE><CHOICE>Error Handling/Exception Handling</CHOICE><CHOICE>Initialization</CHOICE><CHOICE>Installation/Configuration</CHOICE><CHOICE>Performance/Load/Stress</CHOICE><CHOICE>Online help / Error messages</CHOICE><CHOICE>Third Party Software</CHOICE><CHOICE>Not Meeting Coding Standard</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Defect Description",
-            schemaXml: `<Field Type='Note' Name='Defect Description' StaticName='Defect Description' DisplayName='Defect Description' NumLines='6' RichText='FALSE' />`
-        }
-        ,
-        {
-            internalName: "Code Review Checklis",
-            schemaXml: `<Field Type='Text' Name='Code Review Checklis' StaticName='Code Review Checklis' DisplayName='Code Review Checklist/ Coding Standards section ref. no.' MaxLength='255' />`
-        }
-        ,
-        {
-            internalName: "Severity",
-            schemaXml: `<Field Type='Choice' Name='Severity' StaticName='Severity' DisplayName='Severity' Format='Dropdown'><CHOICES><CHOICE>Critical</CHOICE><CHOICE>Moderate</CHOICE><CHOICE>Minor</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Defect Status",
-            schemaXml: `<Field Type='Choice' Name='Defect Status' StaticName='Defect Status' DisplayName='Defect Status' Format='Dropdown'><CHOICES><CHOICE>Open</CHOICE><CHOICE>Closed</CHOICE><CHOICE>Defered</CHOICE><CHOICE>Inprocess</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Review Results",
-            schemaXml: `<Field Type='Choice' Name='Review Results' StaticName='Review Results' DisplayName='Review Results' Format='Dropdown'><CHOICES><CHOICE>Accepted</CHOICE><CHOICE>Rejected</CHOICE><CHOICE>Accepted with Re review</CHOICE><CHOICE>Rejected with Re review</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Defect Origin Phase",
-            schemaXml: `<Field Type='Choice' Name='Defect Origin Phase' StaticName='Defect Origin Phase' DisplayName='Defect Origin Phase' Format='Dropdown'><CHOICES><CHOICE>Requirements Specification</CHOICE><CHOICE>Designs</CHOICE><CHOICE>Code</CHOICE><CHOICE>Unit Testing</CHOICE><CHOICE>Integration Testing</CHOICE><CHOICE>System Testing</CHOICE><CHOICE>Acceptance Testing</CHOICE><CHOICE>Others</CHOICE></CHOICES></Field>`
-        }
-        ,
-        {
-            internalName: "Impacted Components",
-            schemaXml: `<Field Type='Text' Name='Impacted Components' StaticName='Impacted Components' DisplayName='Impacted Components' MaxLength='255' />`
-        }
-        ,
-        {
-            internalName: "Correction / C",
-            schemaXml: `<Field Type='Note' Name='Correction / C' StaticName='Correction / C' DisplayName='Correction / Corrective Action' NumLines='6' RichText='FALSE' />`
-        }
-        ,
-        {
-            internalName: "Planned Closure Date",
-            schemaXml: `<Field Type='DateTime' Name='Planned Closure Date' StaticName='Planned Closure Date' DisplayName='Planned Closure Date' Format='DateOnly' />`
-        }
-        ,
-        {
-            internalName: "Actual Closure Date",
-            schemaXml: `<Field Type='DateTime' Name='Actual Closure Date' StaticName='Actual Closure Date' DisplayName='Actual Closure Date' Format='DateOnly' />`
-        }
-        ,
-        {
-            internalName: "Review Iteration Num",
-            schemaXml: `<Field Type='Number' Name='Review Iteration Num' StaticName='Review Iteration Num' DisplayName='Review Iteration Number' />`
-        }
-        ,
-        {
-            internalName: "Review Completion Da",
-            schemaXml: `<Field Type='DateTime' Name='Review Completion Da' StaticName='Review Completion Da' DisplayName='Review Completion Date' Format='DateOnly' />`
-        }
-        ,
-        {
-            internalName: "Location of defect (",
-            schemaXml: `<Field Type='Text' Name='Location of defect (' StaticName='Location of defect (' DisplayName='Location of defect (Sub section - Line Number)' MaxLength='255' />`
-        }
-        ,
-        {
-            internalName: "Remarks",
-            schemaXml: `<Field Type='Text' Name='Remarks' StaticName='Remarks' DisplayName='Remarks' MaxLength='255' />`
-        }
-    ];
-}
+type CodeReviewDefectsViewField = CodeReviewDefectsFieldName | "LinkTitle";
 
-const defaultViewFields: readonly CodeReviewDefectsViewField[] = [
-    "Code File Cla",
-    "Code File Cla0",
-    "Code File -",
-    "Reviewer Name",
-    "Identified Date",
-    "Defect Type",
-    "Defect Classification",
-    "Defect Description",
-    "Code Review Checklis",
-    "Severity",
-    "Defect Status",
-    "Review Results",
-    "Defect Origin Phase",
-    "Impacted Components",
-    "Correction / C",
-    "Planned Closure Date",
-    "Actual Closure Date",
-    "Review Iteration Num",
-    "Review Completion Da",
-    "Location of defect (",
-    "Remarks"
+const fieldDefinitions: readonly FieldDefinition<CodeReviewDefectsFieldName>[] = [
+	{
+		internalName: "CodeFileClassName",
+		schemaXml: `<Field Type='Text' Name='CodeFileClassName' StaticName='CodeFileClassName' DisplayName='CodeFileClassName' MaxLength='255' />`
+	},
+	{
+		internalName: "CodeFileClassSize",
+		schemaXml: `<Field Type='Text' Name='CodeFileClassSize' StaticName='CodeFileClassSize' DisplayName='CodeFileClassSize' MaxLength='255' />`
+	},
+	{
+		internalName: "CodeFileAuthorDeveloperName",
+		schemaXml: `<Field Type='User' Name='CodeFileAuthorDeveloperName' StaticName='CodeFileAuthorDeveloperName' DisplayName='CodeFileAuthorDeveloperName' UserSelectionMode='PeopleOnly' Mult='TRUE' />`
+	},
+	{
+		internalName: "ReviewerName",
+		schemaXml: `<Field Type='User' Name='ReviewerName' StaticName='ReviewerName' DisplayName='ReviewerName' UserSelectionMode='PeopleOnly' />`
+	},
+	{
+		internalName: "ReviewIterationNumber",
+		schemaXml: `<Field Type='Number' Name='ReviewIterationNumber' StaticName='ReviewIterationNumber' DisplayName='ReviewIterationNumber' />`
+	},
+	{
+		internalName: "IdentifiedDate",
+		schemaXml: `<Field Type='DateTime' Name='IdentifiedDate' StaticName='IdentifiedDate' DisplayName='IdentifiedDate' Format='DateOnly' />`
+	},
+	{
+		internalName: "DefectType",
+		schemaXml: `<Field Type='Choice' Name='DefectType' StaticName='DefectType' DisplayName='DefectType' Format='Dropdown'><CHOICES><CHOICE>Function</CHOICE><CHOICE>Interface</CHOICE><CHOICE>Checking</CHOICE><CHOICE>Assignment</CHOICE><CHOICE>Timing/ Serialization</CHOICE><CHOICE>Build/ Package</CHOICE><CHOICE>Documentation</CHOICE><CHOICE>Algorithm</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "DefectClassification",
+		schemaXml: `<Field Type='Choice' Name='DefectClassification' StaticName='DefectClassification' DisplayName='DefectClassification' Format='Dropdown'><CHOICES><CHOICE>Design Issues</CHOICE><CHOICE>Data Validation</CHOICE><CHOICE>Logical</CHOICE><CHOICE>Computational</CHOICE><CHOICE>User Interface Presentation</CHOICE><CHOICE>Input/Output</CHOICE><CHOICE>Error Handling/Exception Handling</CHOICE><CHOICE>Initialization</CHOICE><CHOICE>Installation/Configuration</CHOICE><CHOICE>Performance/Load/Stress</CHOICE><CHOICE>Online help / Error messages</CHOICE><CHOICE>Third Party Software</CHOICE><CHOICE>Not Meeting Coding Standard</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "CodeReviewChecklist",
+		schemaXml: `<Field Type='Text' Name='CodeReviewChecklist' StaticName='CodeReviewChecklist' DisplayName='CodeReviewChecklist' MaxLength='255' />`
+	},
+	{
+		internalName: "Severity",
+		schemaXml: `<Field Type='Choice' Name='Severity' StaticName='Severity' DisplayName='Severity' Format='Dropdown'><CHOICES><CHOICE>Critical</CHOICE><CHOICE>Moderate</CHOICE><CHOICE>Minor</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "DefectStatus",
+		schemaXml: `<Field Type='Choice' Name='DefectStatus' StaticName='DefectStatus' DisplayName='DefectStatus' Format='Dropdown'><CHOICES><CHOICE>Open</CHOICE><CHOICE>Closed</CHOICE><CHOICE>Defered</CHOICE><CHOICE>In-Process</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "ReviewResults",
+		schemaXml: `<Field Type='Choice' Name='ReviewResults' StaticName='ReviewResults' DisplayName='ReviewResults' Format='Dropdown'><CHOICES><CHOICE>Accepted</CHOICE><CHOICE>Rejected</CHOICE><CHOICE>Accepted with Re-Review</CHOICE><CHOICE>Rejected with Re-Review</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "DefectOriginPhase",
+		schemaXml: `<Field Type='Choice' Name='DefectOriginPhase' StaticName='DefectOriginPhase' DisplayName='DefectOriginPhase' Format='Dropdown'><CHOICES><CHOICE>Requirements Specification</CHOICE><CHOICE>Designs</CHOICE><CHOICE>Code</CHOICE><CHOICE>Unit Testing</CHOICE><CHOICE>Integration Testing</CHOICE><CHOICE>System Testing</CHOICE><CHOICE>Acceptance Testing</CHOICE><CHOICE>Others</CHOICE></CHOICES></Field>`
+	},
+	{
+		internalName: "ImpactedComponents",
+		schemaXml: `<Field Type='Text' Name='ImpactedComponents' StaticName='ImpactedComponents' DisplayName='ImpactedComponents' MaxLength='255' />`
+	},
+	{
+		internalName: "CorrectionCorrectiveAction",
+		schemaXml: `<Field Type='Note' Name='CorrectionCorrectiveAction' StaticName='CorrectionCorrectiveAction' DisplayName='CorrectionCorrectiveAction' NumLines='6' RichText='FALSE' />`
+	},
+	{
+		internalName: "PlannedClosureDate",
+		schemaXml: `<Field Type='DateTime' Name='PlannedClosureDate' StaticName='PlannedClosureDate' DisplayName='PlannedClosureDate' Format='DateOnly' />`
+	},
+	{
+		internalName: "ActualClosureDate",
+		schemaXml: `<Field Type='DateTime' Name='ActualClosureDate' StaticName='ActualClosureDate' DisplayName='ActualClosureDate' Format='DateOnly' />`
+	},
+	{
+		internalName: "LocationOfDefect",
+		schemaXml: `<Field Type='Text' Name='LocationOfDefect' StaticName='LocationOfDefect' DisplayName='LocationOfDefect' MaxLength='255' />`
+	},
+	{
+		internalName: "Remarks",
+		schemaXml: `<Field Type='Note' Name='Remarks' StaticName='Remarks' DisplayName='Remarks' NumLines='6' RichText='FALSE' />`
+	}
 ] as const;
 
-const viewDefinitions: ReadonlyArray<{
-    title: string;
-    fields: readonly CodeReviewDefectsViewField[];
-    makeDefault?: boolean;
-    includeLinkTitle?: boolean;
-}> = [
-    {
-        title: "All Code Review Defects",
-        fields: defaultViewFields,
-        makeDefault: true,
-        includeLinkTitle: true
-    }
+const defaultViewFields: readonly CodeReviewDefectsViewField[] = [
+	"LinkTitle",
+	"CodeFileClassName",
+	"CodeFileClassSize",
+	"CodeFileAuthorDeveloperName",
+	"ReviewerName",
+	"ReviewIterationNumber",
+	"IdentifiedDate",
+	"DefectType",
+	"DefectClassification",
+	"CodeReviewChecklist",
+	"Severity",
+	"DefectStatus",
+	"ReviewResults",
+	"DefectOriginPhase",
+	"ImpactedComponents",
+	"CorrectionCorrectiveAction",
+	"PlannedClosureDate",
+	"ActualClosureDate",
+	"LocationOfDefect",
+	"Remarks"
+] as const;
+
+const displayNameMappings: ReadonlyArray<{ internalName: CodeReviewDefectsFieldName | "Title"; displayName: string }> = [
+	{ internalName: "Title", displayName: "Requirement ID / Ticket ID" },
+	{ internalName: "CodeFileClassName", displayName: "Code File/ Class Name" },
+	{ internalName: "CodeFileClassSize", displayName: "Code File/ Class Size (Lines of Code)" },
+	{ internalName: "CodeFileAuthorDeveloperName", displayName: "Code File - Author/ Developer Name(s)" },
+	{ internalName: "ReviewerName", displayName: "Reviewer Name" },
+	{ internalName: "ReviewIterationNumber", displayName: "Review Iteration Number" },
+	{ internalName: "IdentifiedDate", displayName: "Identified Date" },
+	{ internalName: "DefectType", displayName: "Defect Type" },
+	{ internalName: "DefectClassification", displayName: "Defect Classification" },
+	{ internalName: "CodeReviewChecklist", displayName: "Code Review Checklist/ Coding Standards section ref. no." },
+	{ internalName: "Severity", displayName: "Severity" },
+	{ internalName: "DefectStatus", displayName: "Defect Status" },
+	{ internalName: "ReviewResults", displayName: "Review Results" },
+	{ internalName: "DefectOriginPhase", displayName: "Defect Origin Phase" },
+	{ internalName: "ImpactedComponents", displayName: "Impacted Components" },
+	{ internalName: "CorrectionCorrectiveAction", displayName: "Correction / Corrective Action" },
+	{ internalName: "PlannedClosureDate", displayName: "Planned Closure Date" },
+	{ internalName: "ActualClosureDate", displayName: "Actual Closure Date" },
+	{ internalName: "LocationOfDefect", displayName: "Location of defect (Sub section - Line Number)" },
+	{ internalName: "Remarks", displayName: "Remarks" }
 ];
 
-async function ensureDefaultViewIncludesFields(
-    list: any,
-    fields: readonly CodeReviewDefectsViewField[]
-): Promise<void> {
-    const defaultView = list.defaultView;
-    let schemaXml = "";
+const definition: ListProvisionDefinition<CodeReviewDefectsFieldName, CodeReviewDefectsViewField> = {
+	title: LIST_TITLE,
+	description: "Code review defects list",
+	templateId: 100,
+	fields: fieldDefinitions,
+	defaultViewFields
+};
 
-    try {
-        schemaXml = await defaultView.fields.getSchemaXml();
-    } catch (error) {
-        console.warn(`Failed to read default view schema for list ${LIST_TITLE}:`, error);
-        return;
-    }
+async function ensureDefaultView(sp: SPFI): Promise<void> {
+	const list = sp.web.lists.getByTitle(LIST_TITLE);
+	const view = list.defaultView;
+	try {
+		if (typeof view.fields?.removeAll === "function") {
+			await view.fields.removeAll();
+		}
+	} catch (error) {
+		console.warn(`Failed to clear default view fields on list ${LIST_TITLE}:`, error);
+	}
 
-    for (const field of fields) {
-        const singleQuoteToken = `Name='${field}'`;
-        const doubleQuoteToken = `Name="${field}"`;
-        if (schemaXml.includes(singleQuoteToken) || schemaXml.includes(doubleQuoteToken)) {
-            continue;
-        }
+	for (const field of defaultViewFields) {
+		try {
+			await view.fields.add(field as any);
+		} catch (error) {
+			console.warn(`Failed to add ${field} to default view on list ${LIST_TITLE}:`, error);
+		}
+	}
+}
 
-        try {
-            await defaultView.fields.add(field as any);
-            schemaXml += singleQuoteToken;
-        } catch (error) {
-            console.warn(`Failed to add field ${field} to default view for list ${LIST_TITLE}:`, error);
-        }
-    }
+async function ensureDisplayNames(sp: SPFI): Promise<void> {
+	const list = sp.web.lists.getByTitle(LIST_TITLE);
+	for (const mapping of displayNameMappings) {
+		try {
+			await list.fields.getByInternalNameOrTitle(mapping.internalName).update({ Title: mapping.displayName });
+		} catch (error) {
+			console.warn(`Failed to set display name for ${mapping.internalName} on list ${LIST_TITLE}:`, error);
+		}
+	}
 }
 
 export async function provisionCodeReviewDefects(sp: SPFI): Promise<void> {
-    const fields = buildFieldDefinitions();
-
-    const definition: ListProvisionDefinition<
-        CodeReviewDefectsFieldName,
-        CodeReviewDefectsViewField
-    > = {
-        title: LIST_TITLE,
-        description: "Code review defects list",
-        templateId: 100,
-        fields,
-        defaultViewFields,
-        views: viewDefinitions
-    };
-
-    await ensureListProvision(sp, definition);
-    const list = sp.web.lists.getByTitle(LIST_TITLE);
-    await list.fields.getByInternalNameOrTitle("Title").update({ Title: "Requirement ID / Ticket ID" });
-    await ensureDefaultViewIncludesFields(list, defaultViewFields);
+	await ensureListProvision(sp, definition);
+	await ensureDefaultView(sp);
+	await ensureDisplayNames(sp);
 }
 
 export default provisionCodeReviewDefects;
